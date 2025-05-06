618e3712316f05a994a85fe74b0fc0bd1af946a2
Leonardo Mesquita <63518838+leopinto718@users.noreply.github.com>
2025-05-06 20:44:35 +0000

    Movimentação de peças de xadrez - Desafio Nível Aventureiro Implementado

---
+++ xadrez.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+//Movimentaçao de peças de xadrez
+
+// Torre
+
+int main (){
+    
+printf("\n");
+printf("Movimentação da Torre: \n");
+
+int t = 0;
+while (t < 5){
+
+    printf("Direita\n");
+
+    t++;
+}
+
+
+// Bispo
+
+printf("\n");
+printf("Movimentação do bispo: \n");
+
+int b = 0;
+
+do{
+    printf("Cima, Direita\n");
+    b++;
+}
+while(b < 5);
+
+// Rainha
+
+printf("\n");
+printf("Movimentação da rainha: \n");
+
+for (int r = 0; r < 8; r++) {
+    printf("Esquerda\n");
+}
+
+//Cavalo 
+// Para implementar o movimento do cavalo vamos usar loops aninhados
+
+printf("\n");
+printf("Movimento do cavalo: \n");
+
+for(int movimentoCompleto = 1; movimentoCompleto == 1; movimentoCompleto--){ //Loop externo
+
+    for(int movimentoPrimeiraFase = 0; movimentoPrimeiraFase < 2; movimentoPrimeiraFase++){ //Loop interno
+
+        printf("Baixo \n");
+    }
+
+printf("Esquerda \n");
+
+}
+
+
+
+return 0;
+} 
